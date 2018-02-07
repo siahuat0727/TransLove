a15a1903a4922fdfe2719a67cbeacce854485098
siahuat0727 <tansiahuat@gmail.com>
2018-02-07 10:39:37 +0800

    update

--- LED_fan2.X/build/default/production/main2.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/main2.d  \
- build/default/production/main2.p1:  \
- main2.c 
--- x-mas_white.X/build/default/production/white_fan.p1.d
+++
@@ -1,3 +0,0 @@
- build/default/production/white_fan.d  \
- build/default/production/white_fan.p1:  \
- white_fan.c 
